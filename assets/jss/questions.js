0e42517ae9547fd4af8a683a7046a2c525b7659c
Ion71229 <ionsimion279@gmail.com>
2021-09-11 09:36:00 +0000

    java script working

---
+++ assets/jss/questions.js
@@ -0,0 +1,59 @@
+//creating an array and passing the number,question,option and answer
+let questions = [
+  {
+      numb: 1,
+      question: "Which is the capital of Ireland?",
+      answer: "Dublin",
+      options: [
+          "Dublin",
+          "Cork",
+          "Galway",
+          "Sligo"
+      ]
+  },
+  {
+    numb: 2,
+    question: "What is the currency of Spain?",
+    answer: "Euro",
+    options: [
+        "Pounds",
+        "Euro",
+        "Lei",
+        "Dollars"
+    ] 
+   },
+   {
+    numb: 3,
+    question: "In what city would you find the Eiffel Tower?",
+    answer: "Paris",
+    options: [
+        "Milano",
+        "Madrid",
+        "London",
+        "Paris"
+    ]
+},
+{
+    numb: 4,
+    question: "What is the tallest mountain in the world?",
+    answer: "Mount Everest",
+    options: [
+        "Mount Kilimanjaro",
+        "Mount Elbrus",
+        "Mount Everest",
+        "Mount Fuji"
+    ]
+},
+{
+    numb: 5,
+    question: "How many continents are there in the world?",
+    answer: "Seven",
+    options: [
+        "Seven",
+        "Five",
+        "Ten",
+        "Two"
+    ]
+},
+
+];
